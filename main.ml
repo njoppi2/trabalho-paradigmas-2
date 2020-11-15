02222d071e1e378a304533fef2b2bc3af9a59625
Nelson Joppi <njoppi2@gmail.com>
2020-11-15 17:53:39 -0300

    Uptade code

--- main.ml
+++ main.ml
@@ -47,11 +47,11 @@ Printf.printf "Popped %d off the stack.\n" s#peek
 let a = input_line stdin;;
 Printf.printf "Popped %s off the stack.\n" a;;
 
-let m = Array.init 9 (fun _ -> input_line stdin)
+let matriz = Array.init 9 (fun _ -> input_line stdin)
  
 *)
 
-let m = [|
+let matriz = [|
 "001005300";
 "050490000";
 "000102064";
@@ -64,34 +64,44 @@ let m = [|
 |];;
 
 
+let int_to_char i = Char.chr (i + 48);;
+
 (* In functional programming languages, loops over data structures can be conveniently factored out into higher-order functions, such as the built-in Array.iter function used by our print function: *)
+let printMatriz() = Array.iter print_endline matriz
 
-let print() = Array.iter print_endline m
 (* The test for validity is performed by looping (using recursion) over i=0..8 and testing the row, column and 3x3 square containing the given coordinate: *)
-
+(* this finishes because once one thing return true in the ||'s, the function stops and returns true *)
 let rec invalid ?(i=0) x y n =
-  i<9 && (m.(y).[i] = n || m.(i).[x] = n ||
-      m.(y/3*3 + i/3).[x/3*3 + i mod 3] = n || invalid ~i:(i+1) x y n)
+  i<9
+  && (matriz.(y).[i] = n
+  || matriz.(i).[x] = n
+  || matriz.(y/3*3 + i/3).[x/3*3 + i mod 3] = n
+  || invalid ~i:(i+1) x y n);;
+
 (* Loops over a semi-inclusive range of consecutive integers [l..u) are factored out into a higher-order fold function: *)
-
 let rec fold f accu l u = if l=u then accu else fold f (f accu l) (l+1) u
-(* The search function simply examines each position on the board in turn, trying the number 1..9 in each unfilled position: *)
 
+(* The search function simply examines each position on the board in turn, trying the number 1..9 in each unfilled position: *)
 let rec search ?(x=0) ?(y=0) f accu = match x, y with
     9, y -> search ~x:0 ~y:(y+1) f accu (* Next row *)
   | 0, 9 -> f accu                      (* Found a solution *)
   | x, y ->
-      if m.(y).[x] <> '0' then search ~x:(x+1) ~y f accu else
-        fold (fun accu n ->
-                let n = Char.chr (n + 48) in
-                if invalid x y n then accu else
-                  (m.(y).[x] <- n;
-                   let accu = search ~x:(x+1) ~y f accu in
-                   m.(y).[x] <- '0';
-                   accu)) accu 1 10
-(* Note that this search function is itself a higher-order fold, accumulating the value accu by applying the given function f to it whenever a solution m is found. *)
+  (* if the current char is different than 0, search the next char (x + 1) *)
+      if matriz.(y).[x] <> '0' then search ~x:(x+1) ~y f accu else
+        fold 
+          (fun accu currentNumber ->
+            let currentChar =  int_to_char currentNumber in
+            if invalid x y currentChar
+              then accu
+              else
+                (matriz.(y).[x] <- currentChar;
+                  let accu = search ~x:(x+1) ~y f accu in
+                  matriz.(y).[x] <- '0';
+                  accu))
+          accu 1 10
+(* Note that this search function is itself a higher-order fold, accumulating the value accu by applying the given function f to it whenever a solution matriz is found. *)
 
 (* The main part of the program uses the search function to accumulate the total number of solutions: *)
 
-let () = Printf.printf "%d solutions\n" (search (fun i -> print(); i+1) 0)
+let () = Printf.printf "%d solutions\n" (search (fun i -> printMatriz(); i+1) 0)
 
